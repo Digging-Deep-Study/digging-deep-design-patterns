99d845e78cbb1d4c4d81e7cb26dd493001bbdbd0
chanwoo040531 <chanwoo040531@gmail.com>
2025-05-25 10:08:41 +0900

    feat: update build configuration with Kotlin JVM and dependencies

--- chanwoo/build.gradle.kts
+++ chanwoo/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    kotlin("jvm") version "2.1.21"
 }
 
 group = "com.practice"
@@ -12,8 +13,13 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.netty:netty-all:4.2.1.Final")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
 }
